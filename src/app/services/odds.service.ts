3ecff6e703ff6d9e9fb75935dea3db18db76891b
Aizee Onot <onotaizee@gmail.com>
2021-09-13 11:09:17 +0100

    Display Optimization changes

--- src/app/services/odds.service.ts
+++ src/app/services/odds.service.ts
@@ -6,32 +6,40 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class OddsService {
+
+  api = 'https://api.football-data.org/v2/competitions/CL/matches?status=SCHEDULED';
+  url = 'http://api.football-data.org/v2/teams/';
+  xAuthToken = '440244150fc84a82b3174d238d6d6659';
+
   options = {
-    url: environment.api,
+    url: this.api,
     dataType: 'json',
     type: 'GET',
-    headers: { 'x-auth-token': environment.xAuthToken }
+    headers: { 'x-auth-token': this.xAuthToken }
   };
 
   picsOptions = {
     url: 'http://api.football-data.org/v2/teams/709',
     dataType: 'json',
     type: 'GET',
-    headers: { 'x-auth-token': '440244150fc84a82b3174d238d6d6659' }
+    headers: { 'x-auth-token': this.xAuthToken }
   };
-
-  url = environment.pics;
 
   constructor(
     private http: HttpClient
   ) { }
 
+  //Get list of matches
   getData(url) {
     return this.http.get(url, this.options);
   }
+
+  //Get home team emblem
   getHomeTeamPic(homeID) {
     return this.http.get(environment.pics+homeID, this.picsOptions);
   }
+
+  //Get away team emblem
   getAwayTeamPic( awayID) {
     return this.http.get(environment.pics+awayID, this.picsOptions);
   }
